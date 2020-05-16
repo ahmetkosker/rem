73296ab484c318730b2e9a96ed4e1ae52996acfe
Ahmet <exarons@hotmail.com>
2020-05-16 20:27:20 +0300

    All tests passed

--- test/rem_test.c
+++ test/rem_test.c
@@ -1,38 +1,39 @@
 #include <stdio.h>
 #include "../src/rem.c"
+#define TEST_FILE "/home/ahmet/grep.txt"
 
 int main()
 {
-    if (search(fopen("/home/ahmet/grep.txt", "r"), "ahmet") != 1)
+    if (search(fopen(TEST_FILE, "r"), "ahmet") != 1)
     {
-        printf("Test failed:Ahmet'i bulamadı.");
+        printf("Test failed:Ahmet'i bulamadı.\n");
         return -1;
     }
-    if (search(fopen("/home/ahmet/grep.txt", "r"), "mehmet") != 0)
+    if (search(fopen(TEST_FILE, "r"), "mehmet") != 0)
     {
-        printf("Test succesful:mehmet bulundu.");
+        printf("Test succesful:mehmet bulundu.\n");
         return 1;
     }
-    if (search(fopen("/home/ahmet/grep.txt", "r"), "sagopa") != 1)
+    if (search(fopen(TEST_FILE, "r"), "sagopa") != 1)
     {
-        printf("Test failed:sagopayı bulamadı.");
+        printf("Test failed:sagopayı bulamadı.\n");
         return -1;
     }
-    if (search(fopen("/home/ahmet/grep.txt", "r"), "akman") != 1)
+    if (search(fopen(TEST_FILE, "r"), "man") != 1)
     {
-        printf("Test failed:akmanı bulamadı.");
+        printf("Test failed:akmanı bulamadı.\n");
         return -1;
     }
-    if (search(fopen("/home/ahmet/grep.txt", "r"), "tuvaletkagidi") != 0)
+    if (search(fopen(TEST_FILE, "r"), "tuvaletkagidi") != 0)
     {
-        printf("Test succesful:tuvaletkagidi bulundu.");
+        printf("Test succesful:tuvaletkagidi bulundu.\n");
         return 1;
     }
-    if (search(fopen("/home/ahmet/grep.txt", "r"), "tarrak ") != 1)
+    if (search(fopen(TEST_FILE, "r"), "rak ") != 1)
     {
-        printf("Test failed:tarrak  bulunmadı.");
+        printf("Test failed:rak  bulunmadı.\n");
         return -1;
     }
-    printf("All tests succesful");
+    printf("All tests succesful.\n");
     return 0;
 }
