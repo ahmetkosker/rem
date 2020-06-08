7425e60f5b5cff6486f3356ea9d9b7472bd2f868
Ahmet <exarons@hotmail.com>
2020-06-08 17:32:52 +0300

    case sensitive added

--- src/rem.c
+++ src/rem.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
-unsigned int match_in_file(FILE *f, char *regex)
+unsigned int match_in_file(FILE *f, char *regex, unsigned int case_sensitive)
 {
     unsigned int count = 0;
     unsigned int where = 0;
     size_t regex_len = strlen(regex);
     char next_char;
+    int temp_char = 0;
     unsigned int automata_failed = 0;
     unsigned int state = 0;
     unsigned int i = 0;
-    int temp = 0;
-    int line_number = 1;
-    int iter = 0;
-    int temp1 = 0;
+    unsigned int temp = 0;
+    unsigned int line_number = 1;
+    unsigned int iter = 0;
+    unsigned int sum = 0;
 
     while (!feof(f)) //Dosya bitene kadar.
     {
@@ -33,15 +34,35 @@ unsigned int match_in_file(FILE *f, char *regex)
         {
             if (state == i) //En son kaldığımız harfe geliyoruz.
             {
-                if (regex[i] == next_char) //Okunan karakter duruma bağlı harfe eşitse.
+                if (case_sensitive == 0)
                 {
-                    state = i + 1; //Bir sonraki harfe geçiyoruz.
+                    if (next_char <= 90 && next_char >= 65)
+                        temp_char = next_char + 32;
+                    else if (next_char <= 122 && next_char >= 97)
+                        temp_char = next_char - 32;
+                    if (regex[i] == next_char || regex[i] == temp_char) //Okunan karakter duruma bağlı harfe eşitse.
+                    {
+                        state = i + 1; //Bir sonraki harfe geçiyoruz.
+                    }
+                    else
+                    {
+                        if (state != 0) //Arka arkaya gelen aynı harfler için automata'nın patlamasına karşı bunu yapıyoruz.
+                            automata_failed = 1;
+                        state = 0;
+                    }
                 }
                 else
                 {
-                    if (state != 0) //Arka arkaya gelen aynı harfler için automata'nın patlamasına karşı bunu yapıyoruz.
-                        automata_failed = 1;
-                    state = 0;
+                    if (regex[i] == next_char) //Okunan karakter duruma bağlı harfe eşitse.
+                    {
+                        state = i + 1; //Bir sonraki harfe geçiyoruz.
+                    }
+                    else
+                    {
+                        if (state != 0) //Arka arkaya gelen aynı harfler için automata'nın patlamasına karşı bunu yapıyoruz.
+                            automata_failed = 1;
+                        state = 0;
+                    }
                 }
                 break;
             }
@@ -49,10 +70,11 @@ unsigned int match_in_file(FILE *f, char *regex)
         if (state == regex_len) //Kelime bulunduysa.
         {
             i = 0;
+            int tempo = temp;
             printf("\e[94;1m%d\e[0m\t", line_number);
-            fseek(f, -temp, SEEK_CUR);
+            fseek(f, -tempo, SEEK_CUR);
             iter = temp - regex_len;
-            temp1 = iter;
+            sum = iter;
             while (next_char != '\n' && next_char != '\0')
             {
                 if (i < iter)
@@ -61,11 +83,28 @@ unsigned int match_in_file(FILE *f, char *regex)
                     printf("%c", next_char);
                     i++;
                 }
-                else if (temp1 < temp)
+                else if (sum < temp)
                 {
-                    next_char = fgetc(f);
-                    printf("\e[32;1m%c\e[0m", next_char);
-                    temp1++;
+                    if (sum == iter)
+                    {
+                        next_char = fgetc(f);
+                        printf("\e[32;1m%c", next_char);
+                        sum++;
+                    }
+                    else if (sum == (temp - 1))
+                    {
+                        next_char = fgetc(f);
+                        printf("%c", next_char);
+                        next_char = fgetc(f);
+                        printf("\e[0m%c", next_char);
+                        sum = sum + 2;
+                    }
+                    else
+                    {
+                        next_char = fgetc(f);
+                        printf("%c", next_char);
+                        sum++;
+                    }
                 }
                 else
                 {
--- test/rem_test.c
+++ test/rem_test.c
@@ -5,19 +5,24 @@
 int main()
 {
 
-    if (match_in_file(fopen(TEST_FILE, "r"), "information") != 4)
+    if (match_in_file(fopen(TEST_FILE, "r"), "information", 0) != 8)
     {
         printf("Test failed:4 tane information bulunmadı.\n");
         return -1;
     }
-    if (match_in_file(fopen(TEST_FILE, "r"), "also") != 6)
+    if (match_in_file(fopen(TEST_FILE, "r"), "also", 0) != 7)
     {
-        printf("Test failed:6 tane also bulunmadı.\n");
+        printf("Test failed:7 tane also bulunmadı.\n");
         return -1;
     }
-    if (match_in_file(fopen(TEST_FILE, "r"), "free") != 16)
+    if (match_in_file(fopen(TEST_FILE, "r"), "free", 1) != 16)
     {
         printf("Test failed:16 tane free bulunmadı.\n");
+        return -1;
+    }
+    if (match_in_file(fopen(TEST_FILE, "r"), "the", 0) != 435)
+    {
+        printf("Test failed:435 tane the bulunmadı.\n");
         return -1;
     }
 
