f4e06df3d2c35b0f0feafe7f20619b09f4ba953f
Ahmet <exarons@hotmail.com>
2020-06-16 01:16:52 +0300

    test

--- src/options.h
+++ src/options.h
@@ -1,5 +1,5 @@
 unsigned char isflag(unsigned char, unsigned char);
-ENABLE_NOTHING = 0;
-ENABLE_COLOR = 1;
-ENABLE_CASE_SENSITIVE = 2;
-ENABLE_LINE_NUMBER = 4;
+unsigned char ENABLE_NOTHING = 0;
+unsigned char ENABLE_COLOR = 1;
+unsigned char ENABLE_CASE_SENSITIVE = 2;
+unsigned char ENABLE_LINE_NUMBER = 4;
