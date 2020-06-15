169aef218f1d1bff4dd7ee3d16d16869bee177a0
Ahmet <exarons@hotmail.com>
2020-06-16 02:21:05 +0300

    test

--- test/rem_test.c
+++ test/rem_test.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-
-int match_in_file(FILE *, char *, unsigned char);
-unsigned char isflag(unsigned char, unsigned char);
-unsigned char ENABLE_NOTHING = 0;
-unsigned char ENABLE_COLOR = 1;
-unsigned char ENABLE_CASE_SENSITIVE = 2;
-unsigned char ENABLE_LINE_NUMBER = 4;
+#include "../src/rem.h"
+#include "../src/options.h"
 
 #define TEST_FILE "test/resources/example.txt"
 
